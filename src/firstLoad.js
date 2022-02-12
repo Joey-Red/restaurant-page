6ac2f36f2a9db04c06f8ee81ef35ccba546bf129
Joey Dalrymple <xjoey96@gmail.com>
2022-02-12 03:02:06 -0700

    Working on layout

--- src/firstLoad.js
+++ src/firstLoad.js
@@ -1,25 +1,36 @@
-import Icon from './sushipic2.jpg'
+// import Icon from './sushipic2.jpg'
 
 export function component(){
   const element = document.createElement('div');
   element.classList.add('header')
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  myIcon.style.width = "100%"
-  myIcon.style.height = "50vh"
-  element.appendChild(myIcon);
+  // const myIcon = new Image();
+  // myIcon.src = Icon;
+  // myIcon.style.width = "100%"
+  // myIcon.style.height = "50vh"
+  // element.appendChild(myIcon);
 
   
-  const main = document.createElement('div')
-  main.classList.add("main");
-  main.textContent = "Welcome to Fish's Sushi, we don't dissapoint.";
-  main.style.fontSize = "2rem"
-  main.style.textAlign = "center"
-  element.appendChild(main)
+  const header = document.createElement('div')
+  header.classList.add("header");
+  header.textContent = "Welcome to Fish's Sushi, we don't dissapoint.";
+  header.style.fontSize = "2rem"
+  header.style.textAlign = "center"
+  document.body.append(header)
 
   const tabs = document.createElement("div");
   tabs.classList.add("tabs");
+  document.body.append(tabs)
+  const tabOne = document.createElement("button");
+  const tabTwo = document.createElement("button");
+  const tabThree = document.createElement("button");
+  tabOne.textContent = "HOME";
+  tabTwo.textContent = "MENU";
+  tabThree.textContent = "CONTACT";
+  tabs.append(tabOne, tabTwo, tabThree)
 
-  return element
+  const mainBody = document.createElement("div");
+  mainBody.classList.add("main");
+  mainBody.textContent="TEST TEXT EXAMPLE TEST EXAMPLE TEXT TEST TEST"
+  document.body.append(mainBody);
 }
-document.body.appendChild(component());
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 //going to import the divs and img and stuff.
 import { component } from "./firstLoad"
 import './style.css'
+import { contentOne } from "./contentOne"
+
 
 component();
+
+//button onclick to display content one?
+contentOne();
